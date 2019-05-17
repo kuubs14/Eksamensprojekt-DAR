50768aaff0399ae12d5ebde627d782064bf0efc8
mads5856 <mads.normann@jubii.dk>
2019-05-17 13:16:45 +0200

    Prøver javascript til on-click af send knap

--- javascript/main.js
+++ javascript/main.js
@@ -3,3 +3,24 @@
 function beskedSendt() {
     alert("Din besked er sendt.");
 }
+
+
+let submitStatus = true;
+
+let visSubmit = function() {
+    
+    if (submitStatus === true){
+        getsubmitSendt.style.visibility = "visible";
+    }
+}
+
+
+
+
+
+
+
+
+function show() {
+    document.getElementById("submitSendt").style.display = 'block';
+}
